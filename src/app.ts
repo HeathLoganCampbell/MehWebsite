6ad1307b0a92ab04ff5d851fb1db0146460957a6
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-13 18:37:32 +1200

    add handlebars as a template view engine

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express = require('express');
+import express from "express";
+import templateEngine from "express-handlebars";
+
+const port = process.env.PORT || 3000;
 
 // Create a new express application instance
 const app: express.Application = express();
 
+app.engine('.hbs', templateEngine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', { layout: "main" });
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+    console.log(`Personal Website listening on port ${port}!`);
 });
